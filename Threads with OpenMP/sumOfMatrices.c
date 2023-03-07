476eb1ccf2bc2ed53703eb0f8d557d2e4015dc9a
Mateus Elias <meap@cin.ufpe.br>
2023-03-07 16:33:41 -0300

    feat(Threads with OpenMP): sum of matrices using two threads

---
+++ Threads with OpenMP/sumOfMatrices.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main() {
+    int A[2][2] = {{1,2},{3,4}};
+    int B[2][2] = {{5,6},{7,8}};
+    int C[2][2];
+    int i, j, tId;
+
+    #pragma omp parallel private(j, tId)
+    {
+        #pragma omp for
+        for(i = 0; i < 2; i++) {
+            for(j = 0; j < 2; j++) {
+                tId = omp_get_thread_num();
+                C[i][j] = A[i][j] + B[i][j];
+                printf("A thread de ID %d calculou C[%d][%d]\n", tId, i, j);
+            }
+        }
+    }
+
+    return 0;
+}
