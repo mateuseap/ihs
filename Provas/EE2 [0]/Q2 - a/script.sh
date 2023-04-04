83ced19d7c2dfd5644c6432386f2d290169d4f28
Mateus Elias <meap@cin.ufpe.br>
2023-04-04 06:24:57 -0300

    feat(Provas): added 'clear' command in shell script files

--- Provas/EE2 [0]/Q2 - a/script.sh
+++ Provas/EE2 [0]/Q2 - a/script.sh
@@ -1,3 +1,4 @@
 nasm -f elf calcularPi.asm
 gcc -m32 -no-pie -o calcularPi calcularPi.c calcularPi.o
+clear
 ./calcularPi
--- Provas/EE2 [1]/Q2 - a & b/script.sh
+++ Provas/EE2 [1]/Q2 - a & b/script.sh
@@ -1,3 +1,4 @@
 nasm -f elf cone.asm
 gcc -m32 -no-pie -o cone cone.c cone.o
+clear
 ./cone
